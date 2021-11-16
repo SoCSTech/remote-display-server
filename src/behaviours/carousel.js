4f326fde7887d2233517c09017c012ef5be98642
blewert <bwilliams@lincoln.ac.uk>
2021-11-16 13:23:32 +0000

    basic working carousel system

--- src/behaviours/carousel.js
+++ src/behaviours/carousel.js
@@ -1,4 +1,5 @@
 const logger = require("winston");
+const carouselData = require("../../config/carousel.json");
 
 module.exports = 
 {
@@ -6,19 +7,28 @@ module.exports =
     {
         //Set web socket server
         this.wss = wss;
-        this.interval = setInterval(this.update, 500);
+        this.interval = setInterval(this.update.bind(this), 500);
 
         //Add close event
         this.wss.on('close', (() => this.close(this.wss)).bind(this));
 
         //Call onConnect callback when connected
         this.wss.on('connection', ((ws) => this.onConnect(ws)).bind(this));
+
+        //Slide index
+        this.slideIndex = 0;
+
+        //Set client id to 0
+        this.newClientID = 0;
     },
 
     onConnect(ws)
     {
         //Set disconnect
         ws.on('close', ((ws) => this.onDisconnect(ws)).bind(this));
+
+        //Set client index
+        ws.carouselIndex = this.newClientID++;
 
         logger.info("connect");
     },
@@ -30,7 +40,25 @@ module.exports =
 
     update()
     {
-        logger.info("uuuuupdaaaaaaaateeeee");
+        //Compute new slide index
+        this.slideIndex = (this.slideIndex + 1) % carouselData.length;
+        
+        //Save this to ctx
+        const ctx = this;
+
+        this.wss.clients.forEach((ws, index) => 
+        {
+            //Offset by this client's ID
+            const thisClientIndex = (this.slideIndex + ws.carouselIndex) % carouselData.length;
+
+            //Get slide data
+            const data = carouselData[thisClientIndex];
+
+            //And send it to the display
+            ws.send(JSON.stringify(data));
+
+            logger.debug(`[send] Send slide ${thisClientIndex} to client ${ws.carouselIndex}`);
+        })
     },
 
     close(wss)
